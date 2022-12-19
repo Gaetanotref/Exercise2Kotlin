222634a9288aa6d92fc5c6270bca8e7095e685a4
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-19 12:15:35 +0100

    Commit init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val number1: Int = 5
+    val number2: Float = 5.05f
+    val number3: Double = 5.6139321
+    val result = number1+number2+number3
+
+    println("The sum of all the numbers is ${result}!")
+}
